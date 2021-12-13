391d6b182abf6aa394ede120ece023df296f7bde
Warley-Juneo <warleyjuneo111@gmail.com>
2021-12-13 16:51:58 -0300

    Project 100%

--- mouse_event.c
+++ mouse_event.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse_event.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wjuneo-f <wjuneo-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wjuneo-f <wjuneo-f@student.42sp.org.brr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 17:57:30 by wjuneo-f          #+#    #+#             */
-/*   Updated: 2021/09/28 17:57:31 by wjuneo-f         ###   ########.fr       */
+/*   Updated: 2021/12/13 16:51:08 by wjuneo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,8 @@ int	mouse_event(int keycode, int x, int y, t_variables *var)
 	(void)x;
 	(void)y;
 	var->i++;
-	if (var->i > 85)
-		var->max_iter += 250;
 	if (var->i > 120)
-		var->max_iter += 500;
-	if (var->i > 180)
-		var->max_iter += 1000;
+		var->max_iter += 250;
 	if (keycode == 5)
 		zoom_aplication(var, .9);
 	if (keycode == 4)
