90c3382356336c5a4b5634649a73b87641e22f47
Warley-Juneo <warleyjuneo111@gmail.com>
2021-12-13 16:58:00 -0300

    Project 100%

--- mouse_event.c
+++ mouse_event.c
@@ -6,7 +6,7 @@
 /*   By: wjuneo-f <wjuneo-f@student.42sp.org.brr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 17:57:30 by wjuneo-f          #+#    #+#             */
-/*   Updated: 2021/12/13 16:51:08 by wjuneo-f         ###   ########.fr       */
+/*   Updated: 2021/12/13 16:57:49 by wjuneo-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	zoom_aplication(t_variables *var, float factor)
 	double	mouse_x_after_zoom;
 	double	mouse_y_after_zoom;
 
-	mlx_mouse_get_pos(var->mlx->mlx_ptr, var->mlx->win, &var->m_x2, &var->m_y2);
+	// mlx_mouse_get_pos(var->mlx->mlx_ptr, var->mlx->win, &var->m_x2, &var->m_y2);
 	mouse_x_before_zoom = ((float)var->m_x2) / var->scale + var->x;
 	mouse_y_before_zoom = ((float)var->m_y2) / var->scale + var->y;
 	var->scale *= factor;
@@ -35,8 +35,6 @@ int	mouse_event(int keycode, int x, int y, t_variables *var)
 	(void)x;
 	(void)y;
 	var->i++;
-	if (var->i > 120)
-		var->max_iter += 250;
 	if (keycode == 5)
 		zoom_aplication(var, .9);
 	if (keycode == 4)
